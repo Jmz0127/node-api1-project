699484e8c9b65c2f6b43948e99cf2543073962a5
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-10 18:31:48 -0500

    set up server in index.js and began importing items in the server.js file

--- api/server.js
+++ api/server.js
@@ -1,3 +1,14 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//import express from 'express' in ES6
+const express = require('express');
+
+//instance of express app
+const server = express();
+
+//need our global middleware!!
+server.use(express.json()); //this parses json from the requests
+
+//endpoints
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
