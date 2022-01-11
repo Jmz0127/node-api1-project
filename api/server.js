c9cb0510e2380360bbd9ce0a0109f7ae758a99d9
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-10 23:16:44 -0500

    get function now retrieves all user info

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 
 //import express from 'express' in ES6
 const express = require('express');
-const User = require('./users/model');
+const User = require('./users/model.js');
 
 //instance of express app
 const server = express();
@@ -13,22 +13,21 @@ server.use(express.json()); //this parses json from the requests
 //endpoints
 
 //GET api/users (fetch all users)
-server.get('/api/users', async (req, res) => {
-	try {
-		const users = await User.findAll();
-		res.json(users);
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-	}
-});
 server.get('/api/users', (req, res) => {
-	User.findAll()
+	User.find()
 		.then((users) => {
 			res.json(users);
 		})
 		.catch((err) => {
-			res.status(500).json({ message: err.message });
+			res.status(500).json({ message: 'error getting users' });
 		});
+});
+
+//catch all statement put after all of the other requests
+server.use('*', (req, res) => {
+	res.status(404).json({
+		message: 'not found'
+	});
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
