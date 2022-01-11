3c051b77c8a7c35b05e86dbdc0d371069299a7bc
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-10 19:53:58 -0500

    server is up and running, first attempt at the GET api of list of users in place

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 
 //import express from 'express' in ES6
 const express = require('express');
+const User = require('./users/model');
 
 //instance of express app
 const server = express();
@@ -10,5 +11,24 @@ const server = express();
 server.use(express.json()); //this parses json from the requests
 
 //endpoints
+
+//GET api/users (fetch all users)
+server.get('/api/users', async (req, res) => {
+	try {
+		const users = await User.findAll();
+		res.json(users);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+server.get('/api/users', (req, res) => {
+	User.findAll()
+		.then((users) => {
+			res.json(users);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: err.message });
+		});
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
